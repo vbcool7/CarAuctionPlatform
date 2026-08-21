d4de11d9684a21f4a6479690ae8eaad3367751b8
vbcool7 <VinodArchanaGithub@123>
2026-08-21 11:35:27 +0530

    added resend for mail

--- API/utils/sendEmail.js
+++ API/utils/sendEmail.js
@@ -9,7 +9,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, html) => {
     try {
         const { data, error } = await resend.emails.send({
-            from: `BidDrive Support <${process.env.EMAIL_USER}>`,
+            from: 'BidDrive Support <onboarding@resend.dev>',
             to: [process.env.EMAIL_USER],
             subject: subject,
             html: html,
